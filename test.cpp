758a1a3d81adf6e4aaa05161c11c5558e186bee0
peibozhao <peibozhao@163.com>
2025-06-18 16:35:11 +0800

    update

--- test.cpp
+++ test.cpp
@@ -1,18 +1,8 @@
-first commit
-master : thrid commit
-zpb1992 : fourth commit
-zpb1992 fet1 : fifth
-zpb1992 fet1 : six
-zpb1992 fet2 : seven
-iiii
-zpb1992 fet1 : eight
-zpb1992 fet1 : nine
-zpb1992 fet3 : ten
-zpb1992 fet4 : 11
-iijldfjdlfj
-dlfjdlfj
-zpb1992 fet4 : 12
-zpb1992 fet4 : 13；
-fdjlfjdlj
-jldfjkfdlfjdlj
-dkfjl
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+std::cout << "hello test" << std::endl;
+  return 0;
+}
